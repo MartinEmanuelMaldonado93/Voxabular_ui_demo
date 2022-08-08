f032a2cacc2b252b7818cb43a195c325760a580e
Martin Emanuel <martinemanuel93@gmail.com>
2022-08-08 16:46:17 -0300

    responsive menu

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const navbar__menu = document.querySelector(".navbar__menu");
 const navbar__social = document.querySelector(".navbar__social");
 
-let toggle = false;
+let toggle = true;
 
 navbar__menu.addEventListener("click", () => {
-    toggle = !toggle;
     toggle
-        ? (navbar__social.style.display = "flex")
-        : (navbar__social.style.display = "none");
+    ? (navbar__social.style.display = "flex")
+    : (navbar__social.style.display = "none");
+    toggle = !toggle;
 });
